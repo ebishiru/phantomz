c2bf284c418bfebb25708a659df53f399770cc95
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2026-01-24 18:56:20 -0500

    Increased size for levelling scene

--- src/scenes/LevellingScene.ts
+++ src/scenes/LevellingScene.ts
@@ -15,30 +15,27 @@ export default class LevellingScene extends Phaser.Scene {
         .setOrigin(0)
 
         //Menu Box
-        this.add.rectangle(width/2, height/2, 320, 220, 0x1e1e1e)
+        this.add.rectangle(width/2, height/2, 350, 440, 0x1e1e1e)
         .setStrokeStyle(2, 0xffffff)
 
-        this.add.text(width/2, height/2 - 60, "LEVEL UP", {
+        this.add.text(width/2, height/2 - 180, "LEVEL UP", {
             fontSize: "24px",
             fontFamily: `"Old English Text MT", Georgia, serif`,
             color: "#ffffff"
         }).setOrigin(0.5)
 
-        const confirm = this.add.text(width/2, height/2 + 60, "OK", {
+        const confirm = this.add.text(width/2, height/2 + 180, "OK", {
             fontSize: "18px",
             fontFamily: `"Old English Text MT", Georgia, serif`,
             backgroundColor: "#ffffff",
             color: "#000000",
             padding: {x: 16, y: 8}
         }).setOrigin(0.5)
-        .setInteractive({ useHandCursor: true})
 
         const confirmLevelUp = () => {
             this.scene.stop()
             this.scene.resume("game")
         }
-
-        confirm.on("pointerdown", confirmLevelUp)
 
         this.enterKey = this.input.keyboard!.addKey(
             Phaser.Input.Keyboard.KeyCodes.ENTER
