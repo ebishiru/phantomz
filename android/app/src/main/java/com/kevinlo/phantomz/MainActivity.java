de4837473925fdbc9ca7883a83077bb2c7481118
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2026-08-13 19:29:41 -0400

    saved fullscreen and minimize behaviour

--- android/app/src/main/java/com/kevinlo/phantomz/MainActivity.java
+++ android/app/src/main/java/com/kevinlo/phantomz/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Window;
 
 import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.getcapacitor.BridgeActivity;
 
@@ -15,7 +16,17 @@ public class MainActivity extends BridgeActivity {
 
         Window window = getWindow();
 
-        // Make the app draw behind the Android system bars
+        // Allow the game to draw behind the system bars
         WindowCompat.setDecorFitsSystemWindows(window, false);
+
+        // Hide status bar + navigation bar
+        hideSystemBars();
+    }
+
+    private void hideSystemBars() {
+        WindowCompat.getInsetsController(
+                getWindow(),
+                getWindow().getDecorView()
+        ).hide(WindowInsetsCompat.Type.systemBars());
     }
 }
