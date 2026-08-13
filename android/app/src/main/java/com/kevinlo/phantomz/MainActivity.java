509239583e70e3adbdf88c2458cfcf40f95fe2f0
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2026-08-13 18:46:46 -0400

    installing capacitor

---
+++ android/app/src/main/java/com/kevinlo/phantomz/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kevinlo.phantomz;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kevinlo.phantomz',
+  appName: 'PhantomZ',
+  webDir: 'dist'
+};
+
+export default config;
