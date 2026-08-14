45071e4608cb6c81776194c3076fb9b5d026da45
Kevin Kar-Ho Lo <kevinkarholo@gmail.com>
2026-08-14 17:57:16 -0400

    added relative dot to assets

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -22,17 +22,17 @@ export default class PreloadScene extends Phaser.Scene {
             this.sound.setVolume(Number(savedVolume));
         }
 
-        this.load.audio("titleMusic", "assets/title-music.mp3")
-        this.load.audio("caveMusic", "assets/cave-music.mp3")
-        this.load.audio("snowMusic", "assets/snow-music.mp3")
-        this.load.audio("towerMusic", "assets/tower-music.mp3")
-        this.load.audio("gameOverMusic", "assets/game-over-music.mp3")
-        this.load.image("audio-icon", "assets/audio-icon.png")
-        this.load.image("mute-icon", "assets/mute-icon.png")
+        this.load.audio("titleMusic", "./assets/title-music.mp3")
+        this.load.audio("caveMusic", "./assets/cave-music.mp3")
+        this.load.audio("snowMusic", "./assets/snow-music.mp3")
+        this.load.audio("towerMusic", "./assets/tower-music.mp3")
+        this.load.audio("gameOverMusic", "./assets/game-over-music.mp3")
+        this.load.image("audio-icon", "./assets/audio-icon.png")
+        this.load.image("mute-icon", "./assets/mute-icon.png")
     }
 
     loadArt() {
-        this.load.spritesheet("main-menu-art", "assets/main-menu-art.png", {
+        this.load.spritesheet("main-menu-art", "./assets/main-menu-art.png", {
             frameWidth: 32,
             frameHeight: 32
         })
@@ -40,7 +40,7 @@ export default class PreloadScene extends Phaser.Scene {
 
     loadCharacters() {
         for (let i = 1; i <= 3; i++) {
-            this.load.spritesheet(`player${i}`, `assets/player${i}.png`, {
+            this.load.spritesheet(`player${i}`, `./assets/player${i}.png`, {
                 frameWidth: 16,
                 frameHeight: 16
             })
@@ -49,19 +49,19 @@ export default class PreloadScene extends Phaser.Scene {
 
     loadBosses() {
         for (let i = 1; i <= 30; i++) {
-            this.load.spritesheet(`boss${i}`, `assets/boss${i}.png`, {
+            this.load.spritesheet(`boss${i}`, `./assets/boss${i}.png`, {
                 frameWidth: 16,
                 frameHeight: 16
             })
         }
 
-        this.load.image("boss12-minion", "assets/boss12-minion.png");
-        this.load.image("boss13-clone", "assets/boss13-clone.png");
-        this.load.image("boss17-minion", "assets/boss17-minion.png");
-        this.load.image("boss18-fakeout", "assets/boss18-fakeout.png");
-        this.load.image("boss18-clone", "assets/boss18-clone.png");
-        this.load.image("boss24-minion", "assets/boss24-minion.png");
-        this.load.image("boss29-minion", "assets/boss29-minion.png");
+        this.load.image("boss12-minion", "./assets/boss12-minion.png");
+        this.load.image("boss13-clone", "./assets/boss13-clone.png");
+        this.load.image("boss17-minion", "./assets/boss17-minion.png");
+        this.load.image("boss18-fakeout", "./assets/boss18-fakeout.png");
+        this.load.image("boss18-clone", "./assets/boss18-clone.png");
+        this.load.image("boss24-minion", "./assets/boss24-minion.png");
+        this.load.image("boss29-minion", "./assets/boss29-minion.png");
     }
 
     loadSkills() {
@@ -89,22 +89,22 @@ export default class PreloadScene extends Phaser.Scene {
         ]
 
         skillKeys.forEach(key => {
-            this.load.image(`${key}-icon`, `assets/${key}-icon.png`)
-            this.load.image(`${key}-vfx`, `assets/${key}-vfx.png`)
+            this.load.image(`${key}-icon`, `./assets/${key}-icon.png`)
+            this.load.image(`${key}-vfx`, `./assets/${key}-vfx.png`)
         })
 
         //Extra skill assets
-        this.load.image("fireball2-vfx", "assets/fireball2-vfx.png")        //CAST ICON
-        this.load.image("volt2-vfx", "assets/volt2-vfx.png")                //CAST ICON
-        this.load.image("skip-icon", "assets/skip-icon.png")                //SKIP ICON
-        this.load.image("reroll-icon", "assets/reroll-icon.png")            //REROLL ICON
-        this.load.image("zephyr2-vfx", "assets/zephyr2-vfx.png")            //THRUST VFX
-        this.load.image("mirage2-vfx", "assets/mirage2-vfx.png")            //AOE VFX
-        this.load.image("quasar2-vfx", "assets/quasar2-vfx.png")            //BEAM VFX
-        this.load.image("quasar3-vfx", "assets/quasar3-vfx.png")            //BEAM RING VFX
-        this.load.image("blitzkrieg2-vfx", "assets/blitzkrieg2-vfx.png")    //AOE VFX
-        this.load.image("kraken2-vfx", "assets/kraken2-vfx.png")            //AOE VFX
-        this.load.image("nexus2-vfx", "assets/nexus2-vfx.png")              //BEAM VFX
+        this.load.image("fireball2-vfx", "./assets/fireball2-vfx.png")        //CAST ICON
+        this.load.image("volt2-vfx", "./assets/volt2-vfx.png")                //CAST ICON
+        this.load.image("skip-icon", "./assets/skip-icon.png")                //SKIP ICON
+        this.load.image("reroll-icon", "./assets/reroll-icon.png")            //REROLL ICON
+        this.load.image("zephyr2-vfx", "./assets/zephyr2-vfx.png")            //THRUST VFX
+        this.load.image("mirage2-vfx", "./assets/mirage2-vfx.png")            //AOE VFX
+        this.load.image("quasar2-vfx", "./assets/quasar2-vfx.png")            //BEAM VFX
+        this.load.image("quasar3-vfx", "./assets/quasar3-vfx.png")            //BEAM RING VFX
+        this.load.image("blitzkrieg2-vfx", "./assets/blitzkrieg2-vfx.png")    //AOE VFX
+        this.load.image("kraken2-vfx", "./assets/kraken2-vfx.png")            //AOE VFX
+        this.load.image("nexus2-vfx", "./assets/nexus2-vfx.png")              //BEAM VFX
     }
 
     loadPassives() {
@@ -122,24 +122,24 @@ export default class PreloadScene extends Phaser.Scene {
         ]
 
         passiveKeys.forEach(key => {
-            this.load.image(`${key}-icon`, `assets/${key}-icon.png`)
+            this.load.image(`${key}-icon`, `./assets/${key}-icon.png`)
         })
 
-        this.load.image("gourmet-vfx", "assets/gourmet-vfx.png")            //BREAD VFX
-        this.load.image("desperation-vfx", "assets/desperation-vfx.png")    //AURA VFX
+        this.load.image("gourmet-vfx", "./assets/gourmet-vfx.png")            //BREAD VFX
+        this.load.image("desperation-vfx", "./assets/desperation-vfx.png")    //AURA VFX
     }
 
     loadUI() {
-        this.load.image("exp-orb", "assets/exp-orb.png")
-        this.load.image("loading-border", "assets/loading-border.png")
-        this.load.image("ready-border", "assets/ready-border.png")
-        this.load.image("settings-icon", "assets/settings-icon.png")
+        this.load.image("exp-orb", "./assets/exp-orb.png")
+        this.load.image("loading-border", "./assets/loading-border.png")
+        this.load.image("ready-border", "./assets/ready-border.png")
+        this.load.image("settings-icon", "./assets/settings-icon.png")
     }
 
     loadEnvironment() {
-        this.load.image("cave", "assets/cave.png")
-        this.load.image("snow", "assets/snow.png")
-        this.load.image("tower", "assets/tower.png")
+        this.load.image("cave", "./assets/cave.png")
+        this.load.image("snow", "./assets/snow.png")
+        this.load.image("tower", "./assets/tower.png")
     }
 
     create() {
